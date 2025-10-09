e9159ba51959f27e87d8bf0b20deb12005acea4f
Ganakv-777 <21f3000911@ds.study.iitm.ac.in>
2025-10-09 09:45:40 -0700

    Update streak.py

--- streak.py
+++ streak.py
@@ -1,15 +1,21 @@
-from typing import List
+import pytest
+from streak import longest_positive_streak
 
-def longest_positive_streak(nums: List[int]) -> int:
-    max_streak = 0
-    current_streak = 0
+def test_empty_list():
+    assert longest_positive_streak([]) == 0
 
-    for n in nums:
-        if n > 0:
-            current_streak += 1
-            if current_streak > max_streak:
-                max_streak = current_streak
-        else:
-            current_streak = 0
+def test_all_positive():
+    assert longest_positive_streak([1, 2, 3, 4]) == 4
 
-    return max_streak
+def test_mixed_values():
+    assert longest_positive_streak([2, 3, -1, 5, 6, 7, 0, 4]) == 3
+
+def test_single_negative_values():
+    assert longest_positive_streak([-1, -2, -3]) == 0
+
+def test_multiple_streaks():
+    assert longest_positive_streak([1, 2, 0, 3, 4, 5, -1, 6]) == 3
+
+def test_zero_breaks_streak():
+    assert longest_positive_streak([1, 2, 0, 3]) == 2
+
